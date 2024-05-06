9d4d3d688b1989a4ec8245b9953ef2dd2167bdb1
vipinethical <kvipin279@gmail.com>
2024-05-06 13:21:32 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import "./index.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Vite and Material-UI!</h1>
+      <Button variant="contained">Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
